72517dd681c42ea98785823c2b657daebb27b725
David Carr <dave@dcblog.dev>
2020-12-21 13:19:32 +0000

    Upgraded to Tailwindcss 2 and added typography and forms.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
+  purge: {
+        content: [
+            './public/*.html',
+        ],
+        options: {
+            defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+        },
+    },
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+        boxShadow: {
+            xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
+            outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+        }
+    },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    cursor: ['responsive', 'disabled'],
+    fontWeight: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'dark'],
+    opacity: ['responsive', 'hover', 'focus', 'disabled', 'dark'],
+    textColor: ['responsive', 'hover', 'focus', 'disabled', 'group-hover', 'focus-within', 'dark'],
+    borderColor: ['responsive', 'hover', 'focus', 'disabled', 'last', 'group-hover', 'focus-within', 'dark'],
+    borderWidth: ['responsive', 'last'],
+    boxShadow: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'dark'],
+    backgroundOpacity: ['responsive', 'hover', 'focus', 'active', 'disabled', 'group-hover', 'dark'],
+    backgroundColor: ['responsive', 'hover', 'disabled', 'focus', 'odd', 'even', 'checked', 'dark'],
+    placeholderColor: ['responsive', 'focus', 'disabled', 'dark'],
+    margin: ['responsive', 'hover', 'focus', 'first'],
+    zIndex: ['hover', 'active'],
+  },
+  plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 }
